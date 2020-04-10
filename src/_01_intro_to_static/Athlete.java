eb48a62e62e97e633eb40094671599cf9f580cf8
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-04-10 16:21:25 -0700

    Athlete

--- src/_01_intro_to_static/Athlete.java
+++ src/_01_intro_to_static/Athlete.java
@@ -1,36 +1,53 @@
 package _01_intro_to_static;
 
 public class Athlete {
-	static String raceLocation = "New York";
-	static String raceStartTime = "9.00am";
-	static String name1;
-	static String name2;
-	static int speed1;
-	static int speed2;
+	static String name;
+	static int speed;
+	static int athleteNumber;
 	static int bibNumber = 1;
-	static int nextBibNumber = bibNumber + 1;
+    static int nextBibNumber = bibNumber+1;
+    static String raceLocation = "New York";
+    static String raceStartTime = "9.00am";
 
-	private static void Athlete1(String name1, int speed1) {
-		
-	}
-	public static void Athlete2 (String name2, int speed3) {
-		
-	}
+    Athlete (String name, int speed, int athleteNumber){
+        this.name = name;
+        this.speed = speed;
+        this.athleteNumber = athleteNumber;
+    }
+    public String getName() {
+    	return name;
+    }
+    public int getBibNumber() {
+    	if (athleteNumber == 1) {
+        	bibNumber = bibNumber;
+    	}
+    	else if (athleteNumber == 2){
+    		bibNumber = nextBibNumber;
+    	}
+    		return bibNumber;
+    }
+    public String getRaceLocation() {
+    	return raceLocation;
+    }
 
-	public static void main(String[] args) {
-		// create two athletes
-		// print their names, bibNumbers, and the location of their race.
-		Athlete1("Jeff", 5);
-		System.out.println(name1);
-		System.out.println(speed1);
-		System.out.println(bibNumber);
-		System.out.println(raceLocation);
-		Athlete2("John", 5);
-		System.out.println(name2);
-		System.out.println(nextBibNumber);
-		System.out.println(raceLocation);
 
-	}
-
+    public static void main(String[] args) {
+        //create two athletes
+        //print their names, bibNumbers, and the location of their race. 
+    	Athlete Jeff = new Athlete("Jeff",5,1);
+    	Jeff.getName();
+    	System.out.println(name);
+    	Jeff.getBibNumber();
+    	System.out.println(bibNumber);
+    	Jeff.getRaceLocation();
+    	System.out.println(raceLocation);
+    	
+    	Athlete John = new Athlete("John",5,2);
+    	John.getName();
+    	System.out.println(name);
+    	John.getBibNumber();
+    	System.out.println(bibNumber);
+    	John.getRaceLocation();
+    	System.out.println(raceLocation);
+    }
 }
- 
