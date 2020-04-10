9794922643e35d341204d094a61b3c6f773d8ded
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-04-10 16:52:19 -0700

    Backpack

--- src/_02_backpack/Backpack.java
+++ src/_02_backpack/Backpack.java
@@ -13,7 +13,14 @@ public class Backpack {
 		// Your mission is to go to school.
 		// 1. First you need to put all your supplies into your backpack - use
 		// the putInBackpack(...) methods
-
+		putInBackpack(pencil);
+		putInBackpack(ruler);
+		putInBackpack(textbook);
+		log(pencil);
+		log(pencil);
+		log(pencil);
+		log(ruler);
+		log(textbook);
 		goToSchool();
 	}
 
