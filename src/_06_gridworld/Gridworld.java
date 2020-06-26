b73212a89a789d73d67688f70925b4a58b88707e
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-06-26 16:13:17 -0700

    Gridworld(IP)

--- src/_06_gridworld/Gridworld.java
+++ src/_06_gridworld/Gridworld.java
@@ -1,14 +1,16 @@
 package _06_gridworld;
 
 public class Gridworld {
+	public World();
+	public void show();
+	public void add(32,32,Bug);
+	
+	public Bug();
+	public Bug();
+	public void turn();
+	
 	public static void main(String[] args) {
-		public World();
-		public void show();
-		public void add(32,32,Bug);
-		
-		public Bug();
-		public Bug();
-		public void turn();
+	
 	}
 }
 
